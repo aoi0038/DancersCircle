1d972028e2ef340a2590c4cc6f01e8da97052bd9
大宮　葵 <aoi0038@gmail.com>
2019-07-29 04:18:54 +0000

    Develop Term

--- app/Http/Controllers/Admin/DancersController.php
+++ app/Http/Controllers/Admin/DancersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Dancers;
+
 
 class DancersController extends Controller
 {
@@ -15,7 +17,31 @@ class DancersController extends Controller
     
     public function create(Request $request)
     {
-        return redirect('admin/dancers/create');
+        // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Dancers::$rules);
+
+      $dancers = new Dancers;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$dancers->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $dancers->image_path = basename($path);
+      } else {
+          $dancers->image_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $dancers->fill($form);
+      $dancers->save();
+      
+      return redirect('admin/dancers/create');
     }
     
     public function edit()
--- resources/views/admin/dancers/create.blade.php
+++ resources/views/admin/dancers/create.blade.php
@@ -16,15 +16,23 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">タイトル</label>
+                        <label class="col-md-2" for="name">ダンサーネーム</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
+                    
                     <div class="form-group row">
-                        <label class="col-md-2" for="body">本文</label>
+                        <label class="col-md-2" for="genre">ジャンル</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                            <input type="text" class="form-control" name="genre" value="{{ old('genre') }}">
+                        </div>
+                    </div>
+        
+                    <div class="form-group row">
+                        <label class="col-md-2" for="tweet">つぶやき</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="tweet" rows="20">{{ old('tweet') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -33,57 +41,6 @@
                             <input type="file" class="form-control-file" name="image">
                         </div>
                     </div>
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                                        <div class="form-group row">
-                        <label class="col-md-2" for="name">氏名(name)</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
-                        </div>
-                    </div>
-                    
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2" for="gender">性別(gender)</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
-                        </div>
-                    </div>
-                    
-                    
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2" for="hobby">趣味(hobby)</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
-                        </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2" for="introduction">自己紹介欄(introduction)</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
-                        </div>
-                    </div>  
-
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="更新">
                 </form>
