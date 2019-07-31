eb43aeb3571f04fd19183024eb08c93cfa0e2cb0
大宮　葵 <aoi0038@gmail.com>
2019-07-31 04:55:51 +0000

    Develop Term

--- resources/views/admin/dancers/category.blade.php
+++ resources/views/admin/dancers/category.blade.php
@@ -5,6 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
+                <h1>Welcom to "DancersCircle!!"</h1>
                 <h2>ダンスをする目的を選択してください</h2>
                 <form action="{{ action('Admin\DancersController@create') }}" method="post" enctype="multipart/form-data">
 
@@ -15,14 +16,10 @@
                             @endforeach
                         </ul>
                     @endif
-                        <div id="menu">
-                        <ul>
-                        <li><a href="#">プロダンサーになる</a></li>
-                        <li><a href="#">発表会に出演する</a></li>
-                        <li><a href="#">趣味として</a></li>
-                        <li><a href="#">エクササイズとして</a></li>
-                        </ul> 
-                        </div>
+                        <a href="#" class="btn-circle-flat">プロダンサーになる</a>
+                        <a href="#" class="btn-circle-flat">発表会に出演する</a>
+                        <a href="#" class="btn-circle-flat">趣味として</a>
+                        <a href="#" class="btn-circle-flat">エクササイズとして</a>
 
                     {{ csrf_field() }}
                 </form>
